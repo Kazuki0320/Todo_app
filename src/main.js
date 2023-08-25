42fe65b039c3d6a67e4bc0eaffdea0bbebabfac8
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-08-25 17:55:25 +0900

    コンフリクト解消1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
+
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
@@ -15,5 +16,6 @@ const vuetify = createVuetify({
 const app = createApp(App);
 app.use(store);
 app.use(vuetify);
+app.use(router);
 app.mount('#app');
 
